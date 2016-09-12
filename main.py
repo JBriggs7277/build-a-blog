cd60a74c57d73ae3dfce2be4ef8ba4773d0f38bd
Jeremy Briggs <Jbriggs7277@gmail.com>
2016-09-12 17:13:58 -0500

    pre-hacker fin

--- main.py
+++ main.py
@@ -1,25 +1,74 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Post(db.Model):
+    title = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainBlog(Handler):
+    def render_mainBlog(self, posts=""):
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 5")
+
+        self.render("mainBlog.html", posts = posts)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_mainBlog()
+
+class NewPost(Handler):
+    def render_newPost(self, title="", body="", error=""):
+        self.render("newPost.html", title=title, body=body, error=error)
+
+    def get(self):
+        self.render_newPost()
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if title and body:
+            p = Post(title = title, body = body)
+            p.put()
+
+            self.redirect("/blog/{}".format(p.key().id()))
+
+        else:
+            error = "Please give your post some content and a title!!"
+            self.render_newPost(title, body, error)
+
+class ViewPostHandler(Handler):
+
+    def get(self, post_id):
+        post = Post.get_by_id(int(post_id))
+
+        if not post:
+            error = "Sorry, there is no post at this location.  Please try a different ID."
+            self.response.write(error)
+            return
+
+        self.render('viewPost.html', post = post)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/blog', MainBlog),
+    ('/blog/newpost', NewPost),
+    webapp2.Route('/blog/<post_id:\d+>', ViewPostHandler)
 ], debug=True)
